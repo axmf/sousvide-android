9a3155b1b5f064d9dcbc0516a4a9433c6973ea17
Lionel <lionel.cichero@gmail.com>
2016-03-27 03:48:18 -0300

    sousvide

--- SousVide/app/src/main/java/org/lcichero/cookdivision/sousvide/activity/MainActivity.java
+++ SousVide/app/src/main/java/org/lcichero/cookdivision/sousvide/activity/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +17,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.ivbaranov.rxbluetooth.Action;
+import com.github.ivbaranov.rxbluetooth.RxBluetooth;
+
 import org.lcichero.cookdivision.sousvide.R;
 
 import java.io.IOException;
@@ -21,10 +27,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends Activity {
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final String TAG = "SOUSVIDE_BT";
-    private static String btAddress = "00:12:08:20:08:04";
+    // private static String btAddress = "00:12:08:20:08:04"; // Arduino
+    private static String btAddress = "74:DE:2B:42:92:9D"; // Pc Server
 
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothSocket mmSocket;
@@ -46,6 +58,23 @@ public class MainActivity extends Activity {
     private float setPoint = 60F;
     private final String CELSIUS_DEGREE_NOTATION = " " + (char) 0x00B0 + "C";
 
+    //The BroadcastReceiver that listens for bluetooth broadcasts
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                Log.e("", "device has been connected.");
+                initializeBluetooth();
+            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                Log.e("", "device has been disconnected.");
+                textViewBluetoothStatusValue.setText("disconnected");
+                textViewBluetoothStatusValue.setTextColor(Color.RED);
+            }
+        }
+    };
+
     public void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
         setContentView(R.layout.activity_main);
@@ -61,10 +90,14 @@ public class MainActivity extends Activity {
         this.buttonSubtractDegree.setOnClickListener(new View.OnClickListener() {
             public void onClick(View paramAnonymousView) {
                 try {
-                    setPoint--;
-                    textViewSetPointValue.setText(String.format("%.2f", setPoint) + CELSIUS_DEGREE_NOTATION);
-                    sendData(String.valueOf(String.format("%.2f", setPoint)));
-                } catch (IOException ex) {
+                    if(!(setPoint - 1 <= 0)) {
+                        setPoint--;
+                        textViewSetPointValue.setText(String.format("%.2f", setPoint) + CELSIUS_DEGREE_NOTATION);
+                        sendData(encryptSetPoint(setPoint));
+                    } else {
+                        Toast.makeText(MainActivity.this.getBaseContext(), "Para papu, no te puedo congelar el agua viteh.", Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception ex) {
                     Log.e(TAG, ex.getMessage(), ex);
                     Toast.makeText(MainActivity.this.getBaseContext(), "ERROR! Couldn't send data to SousVide device.", Toast.LENGTH_LONG).show();
                 }
@@ -73,15 +106,27 @@ public class MainActivity extends Activity {
         this.buttonAddDegree.setOnClickListener(new View.OnClickListener() {
             public void onClick(View paramAnonymousView) {
                 try {
-                    setPoint++;
-                    textViewSetPointValue.setText(String.format("%.2f", setPoint) + CELSIUS_DEGREE_NOTATION);
-                    sendData(String.valueOf(String.format("%.2f", setPoint)));
-                } catch (IOException ex) {
+                    if(!(setPoint + 1 >= 100)) {
+                        setPoint++;
+                        textViewSetPointValue.setText(String.format("%.2f", setPoint) + CELSIUS_DEGREE_NOTATION);
+                        sendData(encryptSetPoint(setPoint));
+                    } else {
+                        Toast.makeText(MainActivity.this.getBaseContext(), "No monsters, mas de 100 °C el agua hierve, no jodamo.", Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (Exception ex) {
                     Log.e(TAG, ex.getMessage(), ex);
                     Toast.makeText(MainActivity.this.getBaseContext(), "ERROR! Couldn't send data to SousVide device.", Toast.LENGTH_LONG).show();
                 }
             }
         });
+
+        IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
+        IntentFilter filter2 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        IntentFilter filter3 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(mReceiver, filter1);
+        this.registerReceiver(mReceiver, filter2);
+        this.registerReceiver(mReceiver, filter3);
     }
 
     public void onPause() {
@@ -105,7 +150,6 @@ public class MainActivity extends Activity {
     public void onResume() {
         super.onResume();
         initializeBluetooth();
-        addBluetoothDataListener();
     }
 
     private void initializeBluetooth() {
@@ -124,6 +168,7 @@ public class MainActivity extends Activity {
                     mmInputStream = mmSocket.getInputStream();
                     this.textViewBluetoothStatusValue.setText("connected");
                     this.textViewBluetoothStatusValue.setTextColor(Color.GREEN);
+                    addBluetoothDataListener();
                 } catch (IOException ex) {
                     Log.e(TAG, ex.getMessage(), ex);
                     Toast.makeText(MainActivity.this.getBaseContext(), "ERROR! 'initializeBluetooth'", Toast.LENGTH_LONG).show();
@@ -162,7 +207,35 @@ public class MainActivity extends Activity {
 
                                     handler.post(new Runnable() {
                                         public void run() {
-                                            textViewCurrentTempValue.setText(data + CELSIUS_DEGREE_NOTATION);
+                                            try {
+                                                Log.e("JEJE", data);
+                                                String charOne = Character.toString(data.charAt(0));
+                                                String charTwo = Character.toString(data.charAt(data.length() - 1));
+                                                if ("#".equals(charOne) &&
+                                                        "#".equals(charTwo) &&
+                                                        data.length() == 25) {
+                                                    // #78,00&39,00@78,80&39,40#
+                                                    String newData = data.replace("#", "");
+                                                    newData = newData.replace(",", ".");
+                                                    String[] dataArray = newData.split("@");
+
+                                                    String[] setPointArray = dataArray[0].split("&");
+                                                    Float setPointValue = Float.valueOf(setPointArray[0]);
+                                                    Float setPointCheck = Float.valueOf(setPointArray[1]);
+                                                    if((setPointValue / 2) == setPointCheck) {
+                                                        textViewSetPointValue.setText(String.format("%.2f", setPointValue) + CELSIUS_DEGREE_NOTATION);
+                                                    }
+
+                                                    String[] currentTempArray = dataArray[1].split("&");
+                                                    Float currentTempValue = Float.valueOf(currentTempArray[0]);
+                                                    Float currentTempCheck = Float.valueOf(currentTempArray[1]);
+                                                    if((currentTempValue / 2) == currentTempCheck) {
+                                                        textViewCurrentTempValue.setText(String.format("%.2f", currentTempValue) + CELSIUS_DEGREE_NOTATION);
+                                                    }
+                                                }
+                                            } catch(Exception e) {
+                                                Log.e("Error", "Algo salio mal! :S", e);
+                                            }
                                         }
                                     });
                                 } else {
@@ -178,6 +251,10 @@ public class MainActivity extends Activity {
         });
 
         workerThread.start();
+    }
+
+    private String encryptSetPoint(Float setPoint) {
+        return "#" + String.format("%.2f", setPoint) + "&" + String.format("%.2f", setPoint / 2) + "#";
     }
 
     void sendData(String data) throws IOException {
